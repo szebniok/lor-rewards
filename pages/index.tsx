0f8f0edc47d3e08c1b5c9b3fe798fd4bd4a78ad8
Konrad Bochnia <kbochnia@gmail.com>
2020-08-17 21:05:53 +0200

    Add simple view of the rewards

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,26 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { getWeeklyReward, WeeklyReward } from "../lib/propability"
 import { GetStaticProps } from "next";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 interface Props {
   levelRewards: WeeklyReward[];
 }
 
 const Home: FunctionComponent<Props> = ({ levelRewards }) => {
-  console.log(levelRewards)
+  const [level, setLevel] = useState(1);
+  const rewards = levelRewards[level - 1];
 
   return (
     <div>
-      Rewards
+      <input type="range" min="1" max="13" value={level} onChange={e => setLevel(e.target.valueAsNumber)} />
+      <p>champion: {rewards.champion}</p>
+      <p>wildcard: {rewards.champion_wildcard}</p>
+      <p>token: {rewards.champion}</p>
+      <p>common: {rewards.expected_rewards.common}</p>
+      <p>rare: {rewards.expected_rewards.rare}</p>
+      <p>epic: {rewards.expected_rewards.epic}</p>
+      <p>legendary: {rewards.expected_rewards.legendary}</p>
     </div>
   )
 }
