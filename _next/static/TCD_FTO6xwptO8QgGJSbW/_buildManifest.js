9fce6a7684024292add5934b2abd09579562bb08
szebniok <szebniok@users.noreply.github.com>
2020-09-05 17:53:56 +0000

    deploy: 236a8739d090d6e849d64dab133d738662140e22

---
+++ _next/static/TCD_FTO6xwptO8QgGJSbW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-a86f948e220832958d4b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
