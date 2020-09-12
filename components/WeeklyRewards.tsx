989ee9f74493dbf05b606678bc275d71a914a170
Konrad Bochnia <kbochnia@gmail.com>
2020-09-12 20:51:37 +0200

    Display chest rewards inline

--- components/WeeklyRewards.tsx
+++ components/WeeklyRewards.tsx
@@ -1,5 +1,21 @@
 import { WeeklyReward as WeeklyRewardType } from "../lib/probability";
 import { FunctionComponent } from "react";
+import styles from "../styles/WeeklyRewards.module.css";
+
+interface ChestRarityProps {
+    name: string;
+    quantity: number;
+    precision?: number
+}
+
+const ChestRarity: FunctionComponent<ChestRarityProps> = ({ name, quantity, precision = 3 }) => {
+    return (
+        <div className={styles["chest-rarity"]}>
+            <p>{name}</p>
+            <p>{quantity.toFixed(precision)}</p>
+        </div>
+    )
+}
 
 interface Props {
     rewards: WeeklyRewardType;
@@ -8,14 +24,20 @@ interface Props {
 const WeeklyRewards: FunctionComponent<Props> = ({ rewards }) => {
     return (
         <>
+            <div className={styles["chest-rewards"]}>
+                Expected rewards from chests:
+
+                <div className={styles["chest-rarities-container"]}>
+                    <ChestRarity name="common" quantity={rewards.expected_rewards.common} />
+                    <ChestRarity name="rare" quantity={rewards.expected_rewards.rare} />
+                    <ChestRarity name="epic" quantity={rewards.expected_rewards.epic} />
+                    <ChestRarity name="champion" quantity={rewards.expected_rewards.champion} />
+                    <ChestRarity name="shards" quantity={rewards.shards} precision={0} />
+                </div>
+            </div>
             <p>champion: {rewards.champion_card ? "yes" : "no"}</p>
             <p>wildcard: {rewards.champion_wildcard ? "yes" : "no"}</p>
             <p>token: {rewards.expedition_token ? "yes" : "no"}</p>
-            <p>common: {rewards.expected_rewards.common}</p>
-            <p>rare: {rewards.expected_rewards.rare}</p>
-            <p>epic: {rewards.expected_rewards.epic}</p>
-            <p>champion: {rewards.expected_rewards.champion}</p>
-            <p>shards: {rewards.shards}</p>
         </>
     );
 }
