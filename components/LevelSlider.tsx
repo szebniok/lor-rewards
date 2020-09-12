aebe14433256a546f4c2ddb52cf3869b4fb658d9
Konrad Bochnia <kbochnia@gmail.com>
2020-09-12 20:18:14 +0200

    Extend slider to take whole available width

--- components/LevelSlider.tsx
+++ components/LevelSlider.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import styles from "../styles/LevelSlider.module.css";
 
 interface Props {
     level: number;
@@ -9,8 +10,11 @@ const LevelSlider: FunctionComponent<Props> = ({ level, onLevelChange }) => {
     const updateLevel = onLevelChange ?? (() => { });
     return (
         <>
-            <input type="range" min="1" max="13" value={level} onChange={e => updateLevel(e.target.valueAsNumber)} />
-            <input type="number" min="1" max="13" value={level} onChange={e => updateLevel(e.target.valueAsNumber)} />
+            Select your weekly vault level:
+            <div className={styles["inputs-container"]}>
+                <input className={styles["range-input"]} type="range" min="1" max="13" value={level} onChange={e => updateLevel(e.target.valueAsNumber)} />
+                <input className={styles["number-input"]} type="number" min="1" max="13" value={level} onChange={e => updateLevel(e.target.valueAsNumber)} />
+            </div>
         </>
     );
 }
