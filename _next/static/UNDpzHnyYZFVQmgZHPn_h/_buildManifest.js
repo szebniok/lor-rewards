51b51b4576d37c14ac6b548733b984f53655cd18
szebniok <szebniok@users.noreply.github.com>
2020-10-06 20:59:01 +0000

    deploy: 8561b24d80a29bc931aa3d5ee2b193134a517f49

--- _next/static/UNDpzHnyYZFVQmgZHPn_h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fd1333fb9796db2091b54.css","static\u002Fchunks\u002Fpages\u002Findex-047471c7c8b2e29fc625.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/WBJ2fVV0404Dj2bv-3kDr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fd1333fb9796db2091b54.css","static\u002Fchunks\u002Fpages\u002Findex-721615987821a031148e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
