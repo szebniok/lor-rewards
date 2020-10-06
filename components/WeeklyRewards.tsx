5660e79f0c6cdcb761ea5a0bab292a9036dfc6a5
Konrad Bochnia <kbochnia@gmail.com>
2020-10-06 12:46:55 +0200

    Add additional rewards

--- components/WeeklyRewards.tsx
+++ components/WeeklyRewards.tsx
@@ -10,7 +10,7 @@ interface ChestRarityProps {
 
 const ChestRarity: FunctionComponent<ChestRarityProps> = ({ name, quantity, precision = 3 }) => {
     return (
-        <div className={styles["chest-rarity"]}>
+        <div className={styles["reward"]}>
             <p>{name}</p>
             <img src={`/${name}.svg`} alt={name} title={name} />
             <p>{quantity.toFixed(precision)}</p>
@@ -18,17 +18,33 @@ const ChestRarity: FunctionComponent<ChestRarityProps> = ({ name, quantity, prec
     )
 }
 
+const AdditionalReward: FunctionComponent<{ type: "card" | "wildcard" | "token" }> = ({ type }) => {
+    const iconSrc = type == "token" ? "/token.svg" : "champion.svg";
+    const rewardName = type == "card" ? "Random champion card"
+        : type == "wildcard" ? "Champion wildcard"
+            : "Expedition token"
+
+    return (
+        <div className={styles["reward"]}>
+            <img src={iconSrc} alt={rewardName} title={rewardName} />
+            <p>{rewardName}</p>
+        </div>
+    );
+}
+
 interface Props {
     rewards: WeeklyRewardType;
 }
 
 const WeeklyRewards: FunctionComponent<Props> = ({ rewards }) => {
+    const anyAdditionalRewards = rewards.champion_card || rewards.champion_wildcard || rewards.expedition_token;
+
     return (
         <>
-            <div className={styles["chest-rewards"]}>
-                Expected rewards from chests:
+            <div className={styles["rewards-group"]}>
+                <p>Expected rewards from chests:</p>
 
-                <div className={styles["chest-rarities-container"]}>
+                <div className={styles["rewards-list-container"]}>
                     <ChestRarity name="common" quantity={rewards.expected_rewards.common} />
                     <ChestRarity name="rare" quantity={rewards.expected_rewards.rare} />
                     <ChestRarity name="epic" quantity={rewards.expected_rewards.epic} />
@@ -36,9 +52,17 @@ const WeeklyRewards: FunctionComponent<Props> = ({ rewards }) => {
                     <ChestRarity name="shards" quantity={rewards.shards} precision={0} />
                 </div>
             </div>
-            <p>champion: {rewards.champion_card ? "yes" : "no"}</p>
-            <p>wildcard: {rewards.champion_wildcard ? "yes" : "no"}</p>
-            <p>token: {rewards.expedition_token ? "yes" : "no"}</p>
+            {anyAdditionalRewards &&
+                <div className={styles["rewards-group"]}>
+                    <p>You will also recieve:</p>
+
+                    <div className={styles["rewards-list-container"]}>
+                        {rewards.champion_card && <AdditionalReward type="card" />}
+                        {rewards.champion_wildcard && <AdditionalReward type="wildcard" />}
+                        {rewards.expedition_token && <AdditionalReward type="token" />}
+                    </div>
+                </div>
+            }
         </>
     );
 }
