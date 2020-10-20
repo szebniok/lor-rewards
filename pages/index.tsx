ec6e09ae840116947ae84020002b11e1d844d186
Konrad Bochnia <kbochnia@gmail.com>
2020-10-20 22:08:30 +0200

    Add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import styles from "../styles/Home.module.css";
 import { getWeeklyReward, WeeklyReward } from "../lib/probability"
 import { GetStaticProps } from "next";
 import { FunctionComponent, useState } from "react";
@@ -24,10 +25,24 @@ const Home: FunctionComponent<Props> = ({ levelRewards }) => {
                     </Link>
                 </h1>
             </nav>
-            <main>
+            <main className={styles.main}>
                 <LevelSlider level={level} onLevelChange={setLevel} />
                 <WeeklyRewards rewards={rewards} />
             </main>
+            <footer className={`extend ${styles.footer}`}>
+                <div>
+                    <p>
+                        View the source code <a href="https://github.com/szebniok/lor-rewards">on GitHub</a>.
+                        Last updated: <time dateTime="2020-10-20">2020-10-20 (patch 1.12)</time>
+                    </p>
+                    <p>
+                        <a href="/">lor-rewards</a> isn't endorsed by Riot Games and
+                        doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing
+                        Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks
+                        of Riot Games, Inc.
+                    </p>
+                </div>
+            </footer>
         </>
     )
 }
