b4e9f515f73674e3f987d30835dc78364284f83d
Konrad Bochnia <kbochnia@gmail.com>
2020-10-21 11:21:19 +0200

    Fix url inside footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: FunctionComponent<Props> = ({ levelRewards }) => {
                         Last updated: <time dateTime="2020-10-20">2020-10-20 (patch 1.12)</time>
                     </p>
                     <p>
-                        <a href="/">lor-rewards</a> isn't endorsed by Riot Games and
+                        <Link href="/"><a>lor-rewards</a></Link> isn't endorsed by Riot Games and
                         doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing
                         Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks
                         of Riot Games, Inc.
