b1da9e9af8661f2b4fb84adfa2ae23870295ac49
szebniok <szebniok@users.noreply.github.com>
2020-11-12 10:59:30 +0000

    deploy: e1000002e90f5539a488bbb8d7f9f883ce5f5a3a

---
+++ _next/static/Im7XJi42hAs4x1LQRCRNm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F8eee6e75673a8f5cbc9b.css","static\u002Fchunks\u002Fpages\u002Findex-22cd104ba97d49d2a37f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3203e7e37e39af6cd7ee.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
